abf1f3ce879610ecadd8df46432d6f5060807e37
Robert Korzhov <r.korzhoff@gmail.com>
2024-04-09 13:07:25 +0300

    fix: add copyrights

--- shared/lib/toKebabCase.ts
+++ shared/lib/toKebabCase.ts
@@ -1,3 +1,6 @@
+// Copyright 2024 StarfleetAI
+// SPDX-License-Identifier: Apache-2.0
+
 export const toKebabCase = (str: string) => {
   return str
     .replace(/[^a-zA-Z0-9]+/g, '-')
--- shared/lib/toSentenceCase.ts
+++ shared/lib/toSentenceCase.ts
@@ -1,3 +1,6 @@
+// Copyright 2024 StarfleetAI
+// SPDX-License-Identifier: Apache-2.0
+
 export const toSentenceCase = (str: string) => {
   return str
     .replace(/([A-Z])/g, ' $1')
